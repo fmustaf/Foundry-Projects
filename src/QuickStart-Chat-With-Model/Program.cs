3756dde58bb3e93824b3527f70b087ddec1bbbc7
Faisal Mustafa <faisalm@microsoft.com>
2026-01-29 18:25:38 -0600

    Initial monorepo setup with 3 .NET 8 solutions, docs, workflows for actions and shared library

---
+++ src/QuickStart-Chat-With-Model/Program.cs
@@ -0,0 +1,38 @@
+﻿using Azure.AI.Projects;
+using Azure.AI.Projects.OpenAI;
+using Azure.Identity;
+using OpenAI;
+using OpenAI.Responses;
+
+using System;
+using System.Threading.Tasks;
+
+namespace QuickStart_Chat_With_Model
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+#pragma warning disable OPENAI001
+
+            const string projectEndpoint = "https://demofaisalnewui-1423-resource.services.ai.azure.com/api/projects/demofaisalnewui-1423";
+            const string agentName = "AgentBond";
+
+            // Connect to your project using the endpoint from your project page
+            // The AzureCliCredential will use your logged-in Azure CLI identity, make sure to run `az login` first
+            AIProjectClient projectClient = new(endpoint: new Uri(projectEndpoint), tokenProvider: new DefaultAzureCredential());
+
+            AgentRecord agentRecord = projectClient.Agents.GetAgent(agentName);
+            Console.WriteLine($"Agent retrieved (name: {agentRecord.Name}, id: {agentRecord.Id})");
+
+            // Use the agent to generate a response            
+            ProjectResponsesClient responseClient = projectClient.OpenAI.GetProjectResponsesClientForAgent(agentRecord);
+            // Use the agent to generate a response
+            var response = responseClient.CreateResponse("Hello! Tell me some cool Dad jokes.");
+
+            Console.WriteLine(response.Value.GetOutputText());
+        }
+    }
+}
---
+++ src/QuickStart_Chat_With_Agent/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection.Metadata;
+using Azure.AI.Projects;
+using Azure.AI.Projects.OpenAI;
+using Azure.Identity;
+using OpenAI;
+using OpenAI.Responses;
+
+namespace QuickStart_Chat_With_Agent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #pragma warning disable OPENAI001
+            Console.WriteLine("We programatically created the Agent in Microsoft Foundry, lets chat with it!!");
+
+            const string projectEndpoint = "https://demofaisalnewui-1423-resource.services.ai.azure.com/api/projects/demofaisalnewui-1423";
+            const string agentName = "AgentFez";
+            string modelDeploymentName = "gpt-4.1-mini";
+
+            //string projectEndpoint = Environment.GetEnvironmentVariable("PROJECT_ENDPOINT")
+            //    ?? throw new InvalidOperationException("Missing environment variable 'PROJECT_ENDPOINT'");
+            //string modelDeploymentName = Environment.GetEnvironmentVariable("MODEL_DEPLOYMENT_NAME")
+            //    ?? throw new InvalidOperationException("Missing environment variable 'MODEL_DEPLOYMENT_NAME'");
+            //string agentName = Environment.GetEnvironmentVariable("AGENT_NAME")
+            //    ?? throw new InvalidOperationException("Missing environment variable 'AGENT_NAME'");
+
+            AIProjectClient projectClient = new(new Uri(projectEndpoint), new AzureCliCredential());
+
+            // Optional Step: Create a conversation to use with the agent
+            ProjectConversation conversation = projectClient.OpenAI.Conversations.CreateProjectConversation();
+
+            ProjectResponsesClient responsesClient = projectClient.OpenAI.GetProjectResponsesClientForAgent(
+                defaultAgent: agentName,
+                defaultConversationId: conversation.Id);
+
+            // Chat with the agent to answer questions
+            ResponseResult response = responsesClient.CreateResponse("What is the size of France in square miles?");
+            Console.WriteLine(response.GetOutputText());
+
+            // Optional Step: Ask a follow-up question in the same conversation
+            response = responsesClient.CreateResponse("And what is the capital city?");
+            Console.WriteLine(response.GetOutputText());
+        }
+    }
+}
